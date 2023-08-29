11fc1013bf61f00d2fb382c75cf711fee2b21205
Mikolaj Babinski <zeler11@gmail.com>
2023-08-29 15:11:51 +0200

    updated gitignore and created barebones of the app

---
+++ main.py
@@ -0,0 +1,9 @@
+def terminal_data():
+    credit_value = int(input("Początkowa wysokość kredytu: "))
+    interest = float(input("Oprocentowanie: "))
+    installment = int(input("Stała rata: "))
+
+
+
+if __name__ == '__main__':
+    pass
